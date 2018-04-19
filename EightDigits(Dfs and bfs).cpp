ff4f52ee8363c8758930c70edb46b7c8e8a66452
Tiago Marinho <tiago.marinho.797@gmail.com>
2018-04-19 01:29:04 -0300

    Finish, first problem

--- EightDigits(Dfs and bfs).cpp
+++ EightDigits(Dfs and bfs).cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #define DEBUG if(1)
 #define MAXN 50500
-#define MAX 100005
+#define MAX 160005
 #define MAXL 20
 #define MIN -2000000
 #define endl "\n"
@@ -14,7 +14,7 @@ using namespace std;
 #define pb push_back
 #define mp make_pair
 #define sz(a) int(a.size())
-#define lli unsigned long long int
+#define lli long long int
 #define rep(i,a,n) for (int i=a;i<n;i++)
 #define ler(a,n,vec) for(int i=0;i<n;i++)s(a), vec.pb(a)
 typedef vector<int> vi;
@@ -36,6 +36,8 @@ typedef struct State state;
 
 vector<state> pd;
 vector<state> pd2;
+
+queue< pair<state, pair<int, pair<int, int > > > > fila;
 
 int mat[3][3];
 int it;
@@ -66,13 +68,14 @@ bool check2(State b){
 	return false;
 }
 
-void dfs(State state, int x, int y){
+void dfs(State state, int x, int y, int h){
 	
 	if(check2(state)) return ;
 	
 	if(check(state)){
 		cout << "Achou dfs!" << endl;
 		cout << "Iteracoes : " << it << endl;
+		cout << "Altura : " << h << endl;
 		exit(0);
 	}
 	
@@ -86,19 +89,48 @@ void dfs(State state, int x, int y){
 			
 			swap(aux.vec[x][y], aux.vec[xx][yy]);
 			
-			dfs(aux, xx, yy);
+			dfs(aux, xx, yy, h + 1);
 		}
 	}
 }
 
-void bfs(State state, int x, int y){
-	queue< pair<State, pair<int, int> > > q;
-	q.push(mp(state,mp(x, y)));
+void iterativeDfs(State state, int x, int y, int h, int maximo){
+	if(check2(state)) return ;
+
+	if(check(state)){
+		cout << "Achou iterativeDfs!" << endl;
+		cout << "Iteracoes : " << it << endl;
+		cout << "Altura : " << h << endl;
+		exit(0);
+	}
+	if(h == maximo){
+		fila.push(mp(state, mp(x,mp(y,h))));
+		return ;
+	}
+
+	pd.pb(state);
+	it++;
+	for(int i=0;i<4;i++){
+		int xx = x + dx[i];
+		int yy = y + dy[i];
+		if(xx >= 0 and xx < 3 and yy >= 0 and yy < 3){
+			State aux = state;
+			swap(aux.vec[x][y], aux.vec[xx][yy]);
+			iterativeDfs(aux, xx, yy, h+1, maximo);
+		}
+	}
+
+}
+
+void bfs(State state, int x, int y, int hh){
+	queue< pair<State, pair<int, pair<int, int > > > > q;
+	q.push(mp(state,mp(x, mp(y, hh))));
 
 	while(!q.empty()){
 		State aux = q.front().F;
 		int xx = q.front().S.F;
-		int yy = q.front().S.S;
+		int yy = q.front().S.S.F;
+		int h = q.front().S.S.S;
 		q.pop();
 
 		for(int i=0;i<4;i++){
@@ -111,8 +143,9 @@ void bfs(State state, int x, int y){
 				if(check(aux2)){
 					cout << "Achou bfs!" << endl;
 					cout << "Iteracoes : " << it << endl;
+					cout << "Altura : " << h+1 << endl;
 					exit(0);
-				} else if(!check2(aux2)) q.push({aux2,{xxx,yyy}}), pd.pb(aux2), it++;
+				} else if(!check2(aux2)) q.push({aux2,{xxx,{yyy, h+1}}}), pd.pb(aux2), it++;
 			}
 		}
 	}
@@ -132,8 +165,23 @@ int main(){
 		}
 	}
 	
-	// dfs(state, x, y);
-	bfs(state, x, y);
+	// DFS
+	// dfs(state, x, y, 0);
+
+	//BFS
+	bfs(state, x, y, 0);
+
+
+	//ITERATIVE DFS
+	// iterativeDfs(state, x, y, 0, 5);
+	// while(!fila.empty()){
+	// 	State atual = fila.front().F;
+	// 	int xx = fila.front().S.F;
+	// 	int yy = fila.front().S.S.F;
+	// 	int hh = fila.front().S.S.S;	
+	// 	fila.pop();
+	// 	iterativeDfs(atual, xx, yy, hh, hh*2);
+	// }
 
 	return 0;
 }
